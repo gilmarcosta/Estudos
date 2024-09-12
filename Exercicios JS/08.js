8d3799a632761f4c59ed1ed30170c10b8e001dca
Gilmar-Costa <64275456+gilmarcosta@users.noreply.github.com>
2024-09-11 21:22:43 -0300

    aulas

---
+++ Exercicios JS/08.js
@@ -0,0 +1,9 @@
+console.log('o console funcionou corretamente');
+
+var vel = 50.7
+
+if (vel>60){ //condição simples
+    console.log('Você ultrapassou o limite de velocidade. MULTADO')
+}
+console.log('Dirija com segurança, use o cinto!!')
+
---
+++ Exercicios JS/09.js
@@ -0,0 +1,7 @@
+var numero = 50.5
+
+if(numero > 100){
+    console.log('Maior que 100')
+}else{
+    console.log('Menor que 100')
+}
