512161528688606437b7dd4fb8b6d103f9562208
Gilmar-Costa <64275456+gilmarcosta@users.noreply.github.com>
2024-10-15 22:52:24 -0300

    parte 1

---
+++ Exercicios JS/16 exercicio/script.js
@@ -0,0 +1,30 @@
+
+let vetor = {}
+let res = getElementById('res')
+
+function adicionar(){
+    let n = window.document.getElementById('numero')
+    if(n.value.length==0){
+        window.alert('Insira um valor!!')
+    }else{
+        let num = Number(n.value)
+        if(num > 100 || num < 0){
+            window.alert('Insira um valor de 0 a 100!!')
+        }else{
+            vetor = {num}
+            let option = document.createElement(option)
+                option.appendChild(vetor)
+            res.appendChild()
+        }
+    }
+}
+
+function finalizar(){
+    res = innerHTML(`${vetor}`)
+    let contador = 0
+    while(contador < vetor.length-1){
+        res += innerHTML(`${vetor[contador]}`)
+        contador += 1;
+    }
+}
+
