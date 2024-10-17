ecdce00d00487480738140c33207f06ae384889e
Gilmar-Costa <64275456+gilmarcosta@users.noreply.github.com>
2024-10-16 22:35:59 -0300

    modif.

--- Exercicios JS/16 exercicio/script.js
+++ Exercicios JS/16 exercicio/script.js
@@ -1,9 +1,11 @@
+let resposta = window.document.getElementById('res')  //declarar global
+let n = window.document.getElementById('numero')
+let vetor = []  //colchetes ao inves de chaves
+let resul = window.document.getElementById('resultado')
 
-let vetor = {}
-let res = getElementById('res')
+
 
 function adicionar(){
-    let n = window.document.getElementById('numero')
     if(n.value.length==0){
         window.alert('Insira um valor!!')
     }else{
@@ -11,20 +13,22 @@ function adicionar(){
         if(num > 100 || num < 0){
             window.alert('Insira um valor de 0 a 100!!')
         }else{
-            vetor = {num}
-            let option = document.createElement(option)
-                option.appendChild(vetor)
-            res.appendChild()
+            vetor += [num]
+            let op = window.document.createElement('option') //crases ''
+            op.text = innerHTML = ` O numero ${num} foi adicionado` //sem parenteses  
+            res.appendChild(op)
         }
     }
 }
 
+
+
 function finalizar(){
-    res = innerHTML(`${vetor}`)
     let contador = 0
-    while(contador < vetor.length-1){
-        res += innerHTML(`${vetor[contador]}`)
-        contador += 1;
+    while(contador <= vetor.length-1){
+        //window.alert(`${vetor[contador]}`)
+        resul.innerHTML += `  ${vetor[contador]}   `
+        contador ++;
     }
 }
 
