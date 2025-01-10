63ae9139934e90cfd6e21af6e14f9498b0ad7dca
Gilmar-Costa <64275456+gilmarcosta@users.noreply.github.com>
2025-01-10 20:05:28 -0300

    corrigido

--- Exercicios JS/16 exercicio/script.js
+++ Exercicios JS/16 exercicio/script.js
@@ -1,7 +1,8 @@
-let resposta = window.document.getElementById('res')  //declarar global
-let n = window.document.getElementById('numero')
-let vetor = []  //colchetes ao inves de chaves
-let resul = window.document.getElementById('resultado')
+let res= window.document.getElementById('res')  //declarar global
+let num = window.document.getElementById('numero')
+let lista = window.document.getElementById('flista')
+let valores = []  //colchetes ao inves de chaves
+
 
 let agora = new Date()
 let hora = agora.getHours()
@@ -17,79 +18,84 @@ if(hora >= 6 && hora <= 12){
 }
 
 function isnumero(n){
-
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    }else{
+        return false
+    }
 }
 
 function inlista(n,l){
-
+    if(l.indexOf(Number(n)) != -1){ // aula 15 sobre vetores explica
+        return true
+    }else{
+        return false
+    }
 }
 
 function adicionar(){
-    if(n.value.length==0){
-        window.alert('Insira um valor!!')
+    
+    if(isnumero(num.value) && !inlista(num.value, valores)){
+        valores.push(Number(num.value))
+        let item = window.document.createElement('option') //crases ''
+        item.text = ` O numero ${num.value} foi adicionado.` //sem    
+        lista.appendChild(item)
+        res.innerHTML = ''
     }else{
-        if(isnumero(num.value) && ! inlista(num.value, vetor)){
-            let num = Number(n.value)
-            if(num > 100 || num < 0){
-                window.alert('Insira um valor de 0 a 100!!')
-            }else{
-                vetor += [num]
-                let op = window.document.createElement('option') //crases ''
-                op.text = innerHTML = ` O numero ${num} foi adicionado` //sem parenteses  
-                res.appendChild(op)
-                numero.text = ``
-            }
-        }else{
-            window.alert('Valor invalido ou já encontrado na lista.')
-        }
+        window.alert('Valor invalido ou já encontrado na lista.')
     }
+    num.value =''
+    num.focus()
 }
 
 
 
 function finalizar(){
-    resul.innerHTML = ` `
-    let contador = 0
-    let media = 0
-    let maior = 0 //
-    let menor = -1 //
-    let soma = 0 //
-    let quantidade = 0 //
-
-    resul.innerHTML += `\u{1F449} `
-    while(contador <= vetor.length-1){
-        resul.innerHTML += `<strong> ${vetor[contador]} <strong> `
-        contador ++;
-    }
-
-    resul.innerHTML += `<br>`
-
-    contador = 0
-
-    while(contador <= vetor.length-1){
-        
-        soma += Number(vetor[contador])
-        if(vetor[contador] > maior){
-            maior = vetor[contador]
-        }
-        if(vetor[contador] < menor || menor == -1){
-            menor = vetor[contador]
-        }
-        contador ++;
-    }
-    quantidade = contador
-    media = soma/quantidade
-
-    if(contador == 0){
-        resul.innerHTML += `Sem Valores`
+    if(valores.length == 0){
+        window.alert('Adicione valores antes de finalizar!')
     }else{
-        resul.innerHTML += `<p>Ao todo temos ${quantidade} números cadastrados<p>`
-        resul.innerHTML += `<p>O Maior valor informado foi ${maior}</p>`
-        resul.innerHTML += `<p>O Menor valor informado foi ${menor}</p>`
-        resul.innerHTML += `<p>Somando todos os valores temos ${soma}</p>`
-        resul.innerHTML += `<p>A Média dos valores digitados é  ${media}</p>`
-    }
+        resul.innerHTML = ` `
+        let contador = 0
+        let media = 0
+        let maior = 0 //
+        let menor = -1 //
+        let soma = 0 //
+        let quantidade = 0 //
 
+        resul.innerHTML += `\u{1F449} `
+        while(contador <= vetor.length-1){
+            resul.innerHTML += `<strong> ${vetor[contador]} <strong> `
+            contador ++;
+        }
+
+        resul.innerHTML += `<br>`
+
+        contador = 0
+
+        while(contador <= vetor.length-1){
+            
+            soma += Number(vetor[contador])
+            if(vetor[contador] > maior){
+                maior = vetor[contador]
+            }
+            if(vetor[contador] < menor || menor == -1){
+                menor = vetor[contador]
+            }
+            contador ++;
+        }
+        quantidade = contador
+        media = soma/quantidade
+
+        if(contador == 0){
+            resul.innerHTML += `Sem Valores`
+        }else{
+            resul.innerHTML += `<p>Ao todo temos ${quantidade} números cadastrados<p>`
+            resul.innerHTML += `<p>O Maior valor informado foi ${maior}</p>`
+            resul.innerHTML += `<p>O Menor valor informado foi ${menor}</p>`
+            resul.innerHTML += `<p>Somando todos os valores temos ${soma}</p>`
+            resul.innerHTML += `<p>A Média dos valores digitados é  ${media}</p>`
+        }
+    }
     /*
     ao todo temos x numeros cadastrados
     o maior valor informado foi x
